664d3f287b8cbb1c9d4d3f14de9a4da83e120099
devcp <2915346521@qq.com>
2022-05-31 20:25:44 +0800

    修复跑不起来的bug

--- utils/utils_fit.py
+++ utils/utils_fit.py
@@ -10,7 +10,7 @@ from utils.utils_metrics import f_score
 
 
 def fit_one_epoch(model_train, model, loss_history, eval_callback, optimizer, epoch, epoch_step, epoch_step_val, gen, gen_val, Epoch, cuda, dice_loss, focal_loss, cls_weights, num_classes, \
-    fp16, scaler, save_period, loss_history.log_dir(), local_rank=0):
+    fp16, scaler, save_period, save_dir, local_rank=0):
     total_loss      = 0
     total_f_score   = 0
 
@@ -166,10 +166,10 @@ def fit_one_epoch(model_train, model, loss_history, eval_callback, optimizer, ep
         #   保存权值
         #-----------------------------------------------#
         if (epoch + 1) % save_period == 0 or epoch + 1 == Epoch:
-            torch.save(model.state_dict(), os.path.join(loss_history.log_dir(), 'ep%03d-loss%.3f-val_loss%.3f.pth' % (epoch + 1, total_loss / epoch_step, val_loss / epoch_step_val)))
+            torch.save(model.state_dict(), os.path.join(loss_history.log_dir, 'ep%03d-loss%.3f-val_loss%.3f.pth' % (epoch + 1, total_loss / epoch_step, val_loss / epoch_step_val)))
 
         if len(loss_history.val_loss) <= 1 or (val_loss / epoch_step_val) <= min(loss_history.val_loss):
             print('Save best model to best_epoch_weights.pth')
-            torch.save(model.state_dict(), os.path.join(loss_history.log_dir(), "best_epoch_weights.pth"))
+            torch.save(model.state_dict(), os.path.join(loss_history.log_dir, "best_epoch_weights.pth"))
             
-        torch.save(model.state_dict(), os.path.join(loss_history.log_dir(), "last_epoch_weights.pth"))
+        torch.save(model.state_dict(), os.path.join(loss_history.log_dir, "last_epoch_weights.pth"))
